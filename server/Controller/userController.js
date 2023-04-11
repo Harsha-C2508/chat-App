2b6bbf6a42ffb5fa18c10da0958e8db2b9833c3b
Harsha-C2508 <harshac2508@gmail.com>
2023-04-11 23:12:45 +0530

    done with authentication parts

---
+++ server/Controller/userController.js
@@ -0,0 +1,61 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const tokenCreate = require("../Config/tokenCreate");
+
+
+// part for registration 
+const registerUser = asyncHandler(async (req,res) => {
+    const { name, email, password, pic } = req.body;
+
+    if (!email || !password) {
+        res.status(400);
+        throw new Error("please enter all the feilds")
+    }
+
+    const userIn = await User.findOne({ email });
+    if (userIn) {
+        res.status(400);
+        throw new Error("User already exists")
+    }
+    const newUser = await User.create({
+        name,
+        email,
+        password,
+        pic
+    });
+    if (newUser) {
+        res.status(201).json({
+            _id: newUser._id,
+            name:newUser.name,
+            email:newUser.email,
+            pic: newUser.pic,
+            token: tokenCreate(newUser._id)
+        })
+    }
+    else {
+        res.status(400);
+        throw new Error("Failed to create the user")
+    }
+})
+ 
+
+const authUser = asyncHandler(async(req,res) => {
+    const { email, password } = req.body;
+
+    const userIn = await User.findOne({ email });
+
+    if (userIn && (await userIn.matchPassword(password))) {
+         res.json({
+            _id: userIn._id,
+            name: userIn.name,
+            email:userIn.email,
+            pic: userIn.pic,
+            token: tokenCreate(userIn._id)
+        })
+    }
+    else {
+        res.status(401);
+        throw new Error("Invalid Email or password")
+    }
+})
+module.exports = {registerUser,authUser}
--- server/server.js
+++ server/server.js
@@ -1,22 +1,24 @@
 const express = require("express");
-const { data } = require("./data/data"); 
 const dotenv = require("dotenv");
+const { data } = require("./data/data"); 
 const ConnectDB = require("./Config/db");
-const app = express();
+const userRoutes = require("./Routes/userRoutes");
+const { notFound, errorHandler } = require("./Middlewares/errorMiddleware");
+
 dotenv.config()
 
 ConnectDB()
+const app = express();
+
+app.use(express.json())
 app.get("/", (req, res) => {
-    res.send("API is running")
-})
-app.get("/api/chat", (req, res) => {
-    res.send( data )
+    res.send("API of chit chat is running")
 })
 
-app.get("/api/chat/:id", (req, res) => {
-    const singleChat = data.find((c) => c._id === req.params.id);
-    res.send(singleChat);
-})
+app.use('/api/user', userRoutes)
+
+app.use( notFound )
+app.use( errorHandler )
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT , console.log(`server started at ${PORT}`))
